6b985813f086d340c07fc42d197b8c406e29464d
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-03-20 15:15:30 +0300

    program.cs değiştirildi

--- Program.cs
+++ Program.cs
@@ -17,5 +17,21 @@
 
         sonuc = time <= 6 && time < 11 ? "Günaydın" : time < 18 ? "İyi Günler" : " İyi Geceler";
             Console.WriteLine(sonuc);
+
+        string s1 = "Bir sayının Girin: ";
+        int sayi1;
+        sayi1 = Convert.ToInt32(s1);
+
+
+        if(sayi1 >= 2 && sayi1 <= 5)
+            Console.WriteLine("Not Weird");
+        
+        else if(sayi1 >= 6 && sayi1 <= 20)
+            Console.WriteLine("Weird");
+        
+        else
+            Console.WriteLine("Not Weird");
+
+
     }
 }
